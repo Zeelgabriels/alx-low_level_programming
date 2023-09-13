946d5d0854aa8fc925f06ff7984471063fc99cd1
Zeelgabriels <zealgabriels@gmail.com>
2023-09-13 16:43:00 -0700

    Task_0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - This program would print _putchar and a new line
+ * This program is written by Gabriel
+ * Return: This program would return 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
