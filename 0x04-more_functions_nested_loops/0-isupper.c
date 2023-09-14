0c0e573487e6150825035c50ebd59f183a2680ba
Zeelgabriels <zealgabriels@gmail.com>
2023-09-14 12:28:24 -0700

    Task_0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - This program will print uppercase letters
+ * @c: char to check
+ *
+ * Return: This program will return 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
