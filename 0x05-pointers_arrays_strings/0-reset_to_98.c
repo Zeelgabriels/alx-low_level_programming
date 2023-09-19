1f3e8ebc3ba8840b6430b5903b1c63cd9128027a
Zeelgabriels <zealgabriels@gmail.com>
2023-09-19 16:35:23 -0700

    Task_0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This program will update the value of n to 98
+ * @n: input
+ * Return: This program will return n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
