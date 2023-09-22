7b2baa3f18c3121da0eab82a8fe5b6986e8f32f4
Zeelgabriels <zealgabriels@gmail.com>
2023-09-21 17:34:34 -0700

    Task_12. Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -6,46 +6,46 @@
  * starting with the position, then showing the hex content,
  * then displaying printable characters
  * @b: The buffer to be printed
- * @size: This program would return he number of bytes to be printed from the 
+ * @size: This program would return he number of bytes to be printed from the
  * buffer
  */
 
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+int byte, index;
 
-	for (byte = 0; byte < size; byte += 10)
+for (byte = 0; byte < size; byte += 10)
+{
+	printf("%08x: ", byte);
+
+	for (index = 0; index < 10; index++)
 	{
-		printf("%08x: ", byte);
+		if ((index + byte) >= size)
+			printf("  ");
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf("  ");
+		else
+			printf("%02x", *(b + index + byte));
 
-			else
-				printf("%02x", *(b + index + byte));
+		if ((index % 2) != 0 && index != 0)
+			printf(" ");
+	}
 
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
+	for (index = 0; index < 10; index++)
+	{
+	if ((index + byte) >= size)
+		break;
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
+	else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+		printf("%c", *(b + index + byte));
 
-			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
+	else
+		printf(".");
+	}
 
-			else
-				printf(".");
-		}
+	if (byte >= size)
+		continue;
 
-		if (byte >= size)
-			continue;
-
-		printf("\n");
+	printf("\n");
 	}
 
 	if (size <= 0)
