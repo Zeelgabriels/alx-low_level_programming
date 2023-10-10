deeeafa7e6d55f73bd0d4df679cfc618c9560668
Zeelgabriels <zealgabriels@gmail.com>
2023-10-10 15:56:31 -0700

    Task_2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This program will print the name of the file
+ *
+ * Return: Program will return Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
